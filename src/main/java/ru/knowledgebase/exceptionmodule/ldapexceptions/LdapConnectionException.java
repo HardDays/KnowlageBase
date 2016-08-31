9cfc775c0150a055c4b1dc8dcaefeb66f7ed5153
pisatel <vladpisatel@gmail.com>
2016-08-31 14:42:03 +0300

    Merged project commit

--- src/main/java/ru/knowledgebase/exceptionmodule/ldapexceptions/LdapConnectionException.java
+++ src/main/java/ru/knowledgebase/exceptionmodule/ldapexceptions/LdapConnectionException.java
@@ -3,7 +3,7 @@ package ru.knowledgebase.exceptionmodule.ldapexceptions;
 /**
  * Created by vova on 18.08.16.
  */
-public class LdapConnectionException {
+public class LdapConnectionException extends Exception{
     public LdapConnectionException() {
         super("Ldap connection exception!");
     }
